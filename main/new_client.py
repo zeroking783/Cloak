1d07bc6628acf6ecd4e5cf358fbdd0f4a73a20f8
zeroking783 <bakanov.vladimir2016@gmail.com>
2024-10-18 12:23:12 +0300

    test deploy

--- main/new_client.py
+++ main/new_client.py
@@ -96,6 +96,8 @@ async def new_client(best_server):
         current_time = datetime.now()
         paid_up_to_time = current_time + relativedelta(TIME_GET_CLIENT)
 
+        print("Заданное в TIME_GET_CLIENT:", TIME_GET_CLIENT)
+
         info_connections = [client_link, generated_uuid, server_ip, paid_up_to_time]
 
         return info_connections

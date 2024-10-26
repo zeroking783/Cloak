66e025e7fc24fc4dba391ea21b4bdf18ff2c09ae
zeroking783 <bakanov.vladimir2016@gmail.com>
2024-10-26 15:55:07 +0300

    test deploy

--- main/main.py
+++ main/main.py
@@ -432,9 +432,9 @@ async def approve_payment(callback: types.CallbackQuery):
 
 @dp.callback_query(F.data == "admin_and_del")
 async def del_message(callback: types.CallbackQuery):
+    await callback.answer("Это сообщение пропадет, так что пиши сейчас", show_alert=True)
 
     await bot.delete_message(callback.message.chat.id, callback.message.message_id)
-    await callback.answer("Это сообщение пропадет, так что пиши сейчас: @bakvivas", show_alert=True)
 
 @dp.callback_query(F.data == "registration")
 async def registration_procedure(callback: types.CallbackQuery):
